6063b6337c9d90e7da48eaa18dae3955cbbb1ef4
junwookim <junwoo0824@gmail.com>
2023-06-01 15:00:55 +0900

    [feature] 37

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -7,3 +7,10 @@ TEST(PrimeFactorTest, Of1)
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactorTest, Of2)
+{
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
