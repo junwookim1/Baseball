375dc5a6fb81a275dfa9b880ed5d39a1f2c2f493
junwookim <junwoo0824@gmail.com>
2023-06-02 11:34:01 +0900

    Add Zero test cases

--- Sample-Test1/similaritycheckerTest.cpp
+++ Sample-Test1/similaritycheckerTest.cpp
@@ -19,3 +19,21 @@ TEST(SimilarityCheckerTest, CheckLengthSame)
 	s.checkLength(A, B);
 	EXPECT_EQ(s.getResult(), 60);
 }
+
+TEST(SimilarityCheckerTest, CheckLengthZeroPoint)
+{
+	SimilarityChecker s;
+	string A = "A";
+	string B = "BB";
+	s.checkLength(A, B);
+	EXPECT_EQ(s.getResult(), 0);
+}
+
+TEST(SimilarityCheckerTest, CheckLengthZeroPoint2)
+{
+	SimilarityChecker s;
+	string A = "AA";
+	string B = "BBBB";
+	s.checkLength(A, B);
+	EXPECT_EQ(s.getResult(), 0);
+}
