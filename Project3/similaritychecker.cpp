f6e435cb758357392489b6d146fa27bb5c33b7f0
junwookim <junwoo0824@gmail.com>
2023-06-02 11:09:52 +0900

    Clear project

---
+++ Project3/similaritychecker.cpp
@@ -0,0 +1,17 @@
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+
+class SimilarityChecker
+{
+public:
+	SimilarityChecker()
+	{
+	}
+
+
+private:
+
+};
---
+++ Sample-Test1/similaritycheckerTest.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "../Project3/similaritychecker.cpp"
+
+
+TEST(SimilarityCheckerTest, Basic)
+{
+	SimilarityChecker s;
+	EXPECT_EQ(1, 1);
+}
