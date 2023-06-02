c05c8dbb879483778b2b62f77990ab4237ab98a6
junwookim <junwoo0824@gmail.com>
2023-06-02 14:28:37 +0900

    Make public function to private

--- Project3/similaritychecker.cpp
+++ Project3/similaritychecker.cpp
@@ -6,16 +6,6 @@ using namespace std;
 class SimilarityChecker
 {
 public:
-	bool checkZero(size_t bigger, size_t smaller)
-	{
-		return bigger == 0 || smaller == 0 || bigger >= smaller * 2;
-	}
-
-	bool checkMax(size_t bigger, size_t smaller)
-	{
-		return bigger == smaller;
-	}
-
 	int checkLength(const string& a, const std::string& b)
 	{
 		const int MAX = 60;
@@ -27,6 +17,55 @@ public:
 		if (checkZero(bigger, smaller)) return length_result = 0;
 		if (checkMax(bigger, smaller)) return length_result = MAX;
 		return length_result = MAX - ((bigger - smaller) * MAX / smaller);
+	}
+
+	int checkAlpha(const string& a, const std::string& b)
+	{
+		const int MAX = 40;
+
+		assertSmallLetter(a, b);
+
+		set<char> sa;
+		set<char> sb;
+		makeSetAandB(a, b, sa, sb);
+		int sameCnt = getSameCnt(sa, sb);
+
+		return alpha_result = sameCnt * MAX / (sa.size() + sb.size() - sameCnt);
+	}
+
+	void checkAll(const string& a, const std::string& b)
+	{
+		checkLength(a, b);
+		checkAlpha(a, b);
+	}
+
+	int getLengthResult()
+	{
+		return length_result;
+	}
+
+	int getAlphaResult()
+	{
+		return alpha_result;
+	}
+
+	int getResult()
+	{
+		return getLengthResult() + getAlphaResult();
+	}
+
+private:
+	int length_result = 0;
+	int alpha_result = 0;
+
+	bool checkZero(size_t bigger, size_t smaller)
+	{
+		return bigger == 0 || smaller == 0 || bigger >= smaller * 2;
+	}
+
+	bool checkMax(size_t bigger, size_t smaller)
+	{
+		return bigger == smaller;
 	}
 
 	void makeSetAandB(const string& a, const std::string& b, set<char>& sa, set<char>& sb)
@@ -76,43 +115,4 @@ public:
 			throw std::invalid_argument("�ҹ��� ���Ե�");
 		}
 	}
-
-	void checkAlpha(const string& a, const std::string& b)
-	{
-		const int MAX = 40;
-
-		assertSmallLetter(a, b);
-
-		set<char> sa;
-		set<char> sb;
-		makeSetAandB(a, b, sa, sb);
-		int sameCnt = getSameCnt(sa, sb);
-
-		alpha_result = sameCnt * MAX / (sa.size() + sb.size() - sameCnt);
-	}
-
-	void checkAll(const string& a, const std::string& b)
-	{
-		checkLength(a, b);
-		checkAlpha(a, b);
-	}
-
-	int getLengthResult()
-	{
-		return length_result;
-	}
-
-	int getAlphaResult()
-	{
-		return alpha_result;
-	}
-
-	int getResult()
-	{
-		return getLengthResult() + getAlphaResult();
-	}
-
-private:
-	int length_result = 0;
-	int alpha_result = 0;
 };
