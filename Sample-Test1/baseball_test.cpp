19e6c502938fd91177c5cd006a7d1324d8960b57
junwookim <junwoo0824@gmail.com>
2023-06-02 10:15:23 +0900

    Add baseball project

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -2,7 +2,30 @@
 #include "../Project3/baseball.cpp"
 
 
-TEST(BaseballTest, Baseball)
+TEST(BaseballTest, BaseballException)
 {
-	EXPECT_EQ(1, 1);
+	Baseball baseball("123");
+
+	EXPECT_THROW(baseball.guess(""), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1543"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1v3"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("121"), std::invalid_argument);
+}
+
+TEST(BaseballTest, PerfectMatch)
+{
+	Baseball baseball("123");
+	Result result = baseball.guess("123");
+	EXPECT_EQ(result.solved, true);
+	EXPECT_EQ(result.strikes, 3);
+	EXPECT_EQ(result.balls, 0);
+}
+
+TEST(BaseballTest, MissMatch)
+{
+	Baseball baseball("123");
+	Result result = baseball.guess("789");
+	EXPECT_EQ(result.solved, false);
+	EXPECT_EQ(result.strikes, 0);
+	EXPECT_EQ(result.balls, 0);
 }
